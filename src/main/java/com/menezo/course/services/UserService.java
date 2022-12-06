07fba24de8cd1695793fcd7b0f18143f68942a92
Menezo <joaovitormenezesm12@gmail.com>
2022-12-06 16:24:03 -0300

    Exception handling - update

--- src/main/java/com/menezo/course/services/UserService.java
+++ src/main/java/com/menezo/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.menezo.course.repositories.UserRepository;
 import com.menezo.course.services.exceptions.DatabaseException;
 import com.menezo.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,15 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public void updateData(User entity, User obj) {
